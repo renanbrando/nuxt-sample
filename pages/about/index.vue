4a5b32826c5d0f6483a0a8c0bc96409c98460372
Renan Ribeiro Brando <renanbrando@Renans-MacBook-Pro-2.local>
2019-05-18 16:49:57 -0300

    Added project files.

---
+++ pages/about/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h1>Hello from about</h1>
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        display: flex;
+        flex-flow: column;
+        align-content: center;
+        justify-content: center;
+    }
+</style>
